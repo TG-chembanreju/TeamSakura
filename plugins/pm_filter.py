ef461419ee0e189cfe831c9c14f565dba235278e
TG-PushpaReju <97326699+TG-chembanreju@users.noreply.github.com>
2022-02-13 07:35:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -430,8 +430,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons= [[
-            InlineKeyboardButton('𝖲𝗈𝗎𝗋𝖼𝖾', url='https://t.me/ippotharam34'),
-            InlineKeyboardButton('𝖬𝗈𝗏𝗂𝖾𝗌', url='https://t.me/fbm_movies'),
+            InlineKeyboardButton('Repo😈', url='https://t.me/ippotharam34'),
+            InlineKeyboardButton('Files🚫', url='https://t.me/fbm_movies'),
             InlineKeyboardButton('𝖡𝖺𝖼𝗄', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
